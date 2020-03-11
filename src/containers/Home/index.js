99ed55f9090adf02a9a1969603659b7e5b8b3153
Shrutika Shrutika <shrutika051220@gmail.com>
2020-03-11 19:05:01 +0530

    update main page with search

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './style.css';
-import Logo from '../Logo/index'
+import Logo from '../Logo/index';
+import SearchBar from '../SearchBar/index';
+
 
 const Home =(props)=>{
     return(
       <div>
       <div className="text-center">
         <h1> NbliK- Network of Knowledge </h1>
+        <SearchBar/>
         <Logo/>
       </div>
       </div>
