0518d5e5a60a63605ed93a2fc04de764a8b3452c
Shrutika Shrutika <shrutika051220@gmail.com>
2020-03-11 19:07:46 +0530

    add catrgory component

---
+++ src/containers/Category/index.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import categoriesData from '../../api/category.json';
+
+
+
+class Category extends Component {
+  render () {
+    return(
+      <div>
+      <h1>Categories</h1>
+      </div>
+
+)
+  }
+}
+
+export default Category ;
