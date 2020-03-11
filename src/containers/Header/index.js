03f2ae2ad9cbeed26329bc1a97a5719aaf898814
Shrutika Shrutika <shrutika051220@gmail.com>
2020-03-11 17:32:37 +0530

    update search bar

--- src/containers/Header/index.js
+++ src/containers/Header/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './style.css';
 import Logo from '../Logo/index';
+import SearchImg from '../../images/icons/search.png';
+
 
 const Header = props =>{
     return(
     <div className="header">
-    <div className="headerSearch">
-      <img src={require('../../images/icons/search.png')} alt="Search"/>
-      <input type="text" placeholder="search"/>
-    </div>
+      <div className="headerSearch">
+        <form>
+          <img src={SearchImg} alt="Search" style={{height:45}}/>&nbsp;&nbsp;
+          <input type="text" className="input" placeholder="Search anything here!!"/>
+        </form>
+      </div>
       <nav className="headerMenu">
         <a href="http://localhost:3000/blog">Blog</a>
         <a href="http://localhost:3000/discussion">Discussions</a>
