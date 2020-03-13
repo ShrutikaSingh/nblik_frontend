a9eb60b18d54dfa6f485a0209a97b8be350f1bc7
Shrutika Shrutika <shrutika051220@gmail.com>
2020-03-13 13:23:22 +0530

    add Button with props

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -2,15 +2,17 @@ import React from 'react';
 import './style.css';
 import Logo from '../Logo/index';
 import SearchBar from '../SearchBar/index';
-
+import BlogButton from '../UI/Buttons/BlogButton';
 
 const Home =(props)=>{
     return(
       <div>
       <div className="text-center">
-        <h1> NbliK- Network of Knowledge </h1>
-        <SearchBar/>
         <Logo/>
+        <h1> NbliK- Network of Knowledge </h1>
+
+        <BlogButton data="NBLIK" page="home"/>
+
       </div>
       </div>
     )
