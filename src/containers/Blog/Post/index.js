b2d9007347c845e1a887b5b93dabf49a989a1246
Shrutika Shrutika <shrutika051220@gmail.com>
2020-03-13 15:14:48 +0530

    update blog with mapping all posts in a mage with index

--- src/containers/Blog/Post/index.js
+++ src/containers/Blog/Post/index.js
@@ -1,22 +1,46 @@
-import React from 'react'
+import React, {Component} from 'react'
 import './style.css';
-import BlogButton from '../Buttons/BlogButton';
-import postData from '../../../api/category.json';
+import BlogButton from '../../UI/Buttons/BlogButton';
+import postData from '../../../api/posts.json';
 
-
-const Post=(props)=>{
-
-
-
-
+class Post extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      posts:postData
+    }
+  }
+render () {
+  console.log(this.state.posts[0])
   return(
     <div className="card">
-    <h1>dbshds</h1>
-
-    <BlogButton data="Like" page="blog"/>
-    <BlogButton data="Share" page="blog"/>
+    {this.state.posts.map((post,i)=>{return(
+      <div key={i}>
+      <div className="card-header">
+        {this.state.posts[i].category}
+      </div>
+      <div >
+          {this.state.posts[i].title}
+      </div>
+      <div>
+          {this.state.posts[i].username}
+      </div>
+      <div>
+          {this.state.posts[i].postbody}
+      </div>
+      <div>
+          {this.state.posts[i].likes}
+          <BlogButton data="Like" page="blog"/>
+          <BlogButton data="Share" page="blog"/>
+      </div>
+      <div>
+          {this.state.posts[i].postbody}
+      </div>
+      </div>
+    )})}
     </div>
   )
+}
 }
 
 export default Post;
--- src/containers/Blog/index.js
+++ src/containers/Blog/index.js
@@ -1,16 +1,15 @@
 import React, { Fragment} from 'react';
 import './style.css';
 import Header from '../Header/index';
-import BarSection from '../Blog/BlogContnent/index';
 import NavBar from '../NavBar/index';
 import Category from '../Category/index';
-
+import Post from "../Blog/Post/index";
 
 const Blog=props=> {
     return(
       <Fragment>
         <Header/>
-        <BarSection/>
+        <Post/>
         <NavBar/>
         <Category/>
       </Fragment>
