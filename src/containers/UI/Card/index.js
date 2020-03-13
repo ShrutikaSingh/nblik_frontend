6f887ee236b24e3c46ae322d74502cdeb8941957
Shrutika Shrutika <shrutika051220@gmail.com>
2020-03-13 13:24:01 +0530

    update card of main blog page

--- src/containers/UI/Card/index.js
+++ src/containers/UI/Card/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import './style.css';
+import BlogButton from '../Buttons/BlogButton';
 
 const Card=(props)=>{
   return(
     <div className="card">
     <h1>dbshds</h1>
+    <BlogButton data="Like" page="blog"/>
+    <BlogButton data="Share" page="blog"/>
     </div>
   )
 }
