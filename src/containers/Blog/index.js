772b7c050df1d998a03154d96af8f2fe923e1e02
Shrutika Shrutika <shrutika051220@gmail.com>
2020-03-13 13:22:51 +0530

    update

--- src/containers/Blog/index.js
+++ src/containers/Blog/index.js
@@ -4,6 +4,8 @@ import Header from '../Header/index';
 import BarSection from '../BarSection/index';
 import NavBar from '../NavBar/index';
 import Category from '../Category/index';
+
+
 const Blog=props=> {
     return(
       <Fragment>
