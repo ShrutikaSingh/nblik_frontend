e461a1d06a82343fb5bf41bc012820b824804702
Shrutika Shrutika <shrutika051220@gmail.com>
2020-03-13 11:23:51 +0530

    update blog

--- src/containers/Category/index.js
+++ src/containers/Category/index.js
@@ -1,15 +1,19 @@
 import React, {Component} from 'react';
 import categoriesData from '../../api/category.json';
 
-
-
 class Category extends Component {
+  constructor(props){
+    super(props);
+    this.items=categoriesData;
+  }
   render () {
     return(
       <div>
       <h1>Categories</h1>
+      <ul>
+      {this.items.map(item=><li>{item}</li>)}
+      </ul>
       </div>
-
 )
   }
 }
