fa95261565406efcf1d1de85fcba755f9b36e91c
Shrutika Shrutika <shrutika051220@gmail.com>
2020-03-13 17:23:18 +0530

    update post with proper display using bootstrap

--- src/containers/Header/index.js
+++ src/containers/Header/index.js
@@ -1,8 +1,7 @@
 import React, {useState} from 'react';
 import './style.css';
-import Logo from '../Logo/index';
 import SearchImg from '../../images/icons/search.png';
-
+import mainImg from '../../images/nblik.jpg';
 
 const Header = props =>{
 
@@ -16,22 +15,32 @@ const searchSubmit = (e) => {
 const openSearch = () =>{
   setSearch(true);
 }
-
     return(
-    <div className="header">
-      <div className="headerSearch">
-        <form onSubmit={searchSubmit}>
-          <input type="text" className="input" placeholder="Search anything here!!"/>&nbsp;&nbsp;
-          <img onClick={openSearch} src={SearchImg} className="searchIcon" alt="Search" style={{height:45}}/>
-        </form>
+      <div class="row">
+        <div class="col-sm-2">
+          <a href="http://localhost:3000/blog">
+            <img src={mainImg} style={{height:50}} alt=""></img>
+          </a>
+        </div>
+          <div class="col-sm-6">
+            <div className="header">
+              <div className="headerSearch">
+                <form onSubmit={searchSubmit}>
+                  <input type="text" className="input" placeholder="Search anything here!!"/>&nbsp;&nbsp;
+                  <img onClick={openSearch} src={SearchImg} className="searchIcon" alt="Search" style={{height:35}}/>
+                </form>
+              </div>
+            </div>
+        </div>
+        <div class="col-sm-4">
+          <nav className="headerMenu">
+            <a href="http://localhost:3000/blog">Blog</a>
+            <a href="http://localhost:3000/discussion">Discussions</a>
+            <a href="http://localhost:3000/write">Write</a>
+            <a href="http://localhost:3000/register/">Signup</a>
+            <a href="http://localhost:3000/login/">Signin</a>
+          </nav>
       </div>
-      <nav className="headerMenu">
-        <a href="http://localhost:3000/blog">Blog</a>
-        <a href="http://localhost:3000/discussion">Discussions</a>
-        <a href="http://localhost:3000/write">Write</a>
-        <a href="http://localhost:3000/register/">Signup</a>
-        <a href="http://localhost:3000/login/">Signin</a>
-      </nav>
     </div>
   )
 }
