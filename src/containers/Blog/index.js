78cac4b22d7f6321d35a9c152d745bdfce40544e
Shrutika Shrutika <shrutika051220@gmail.com>
2020-03-13 19:06:18 +0530

    update styling of blog

--- src/containers/Blog/index.js
+++ src/containers/Blog/index.js
@@ -3,7 +3,7 @@ import Header from '../Header/index';
 import NavBar from '../NavBar/index';
 import Category from '../Category/index';
 import Post from "../Blog/Post/index";
-
+import Pagination from "../UI/Pagination";
 const Blog=props=> {
     return(
       <Fragment>
@@ -13,13 +13,16 @@ const Blog=props=> {
                   <Category/>
                 </div>
                 <div class="col-sm-8">
+
                   <Post/>
+
                 </div>
                 <div class="col-sm-2">
                   <h3>Column 3</h3>
                   <p>Lorem ipsum dolor..</p>
                 </div>
-          <NavBar/>
+                <NavBar/>
+                <Pagination/>
           </div>
       </Fragment>
   )
